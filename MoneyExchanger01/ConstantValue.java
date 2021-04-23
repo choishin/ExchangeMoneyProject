d0ab1dbf2bb841168b4eae0042ea788e48f85531
choishin <choishin10@gmail.com>
2021-04-23 10:14:40 +0900

    My first Project!

--- MoneyExchanger01/ConstantValue.java
+++ MoneyExchanger01/ConstantValue.java
@@ -6,8 +6,8 @@ public class ConstantValue {
 	final static int EX_type2 = 2;
 	final static int EX_type3 = 3;
 	final static int EX_type0 = 0;
-	final static double EX_RATE1 =1127.86;
-	final static double EX_RATE2 =1327.59;
-	final static double EX_RATE3 =10.20;
+	final static double EX_RATE1 =1119.50; //미국달러환율
+	final static double EX_RATE2 =1345.25; //유로환율
+	final static double EX_RATE3 =1037.40/100; //(<-100엔당 가격)일본엔화환율
 
 }
